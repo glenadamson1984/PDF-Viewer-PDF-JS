df385a5588f92b60208270bca780ab5934535755
glenadamson1984 <glenadamson1984@gmail.com>
2021-03-25 20:59:29 +0000

    finished solution

--- js/main.js
+++ js/main.js
@@ -1,30 +1,91 @@
-const url = '../docs/sample.pdf';
+const url = "../docs/sample.pdf";
 
-let pdfDoc = null, 
-    pageNum = 1, 
-    pageIsRendering = false,
-    pageNumIsPending = false;
+let pdfDoc = null,
+  pageNum = 1,
+  pageIsRendering = false,
+  pageNumIsPending = null;
 
 const scale = 1.5,
-    canvas = document.querySelector('#pdf-render'),
-    cts = canvas.getContext('2d');
+  canvas = document.querySelector("#pdf-render"),
+  ctx = canvas.getContext("2d");
 
 // Render the page
 const renderPage = (num) => {
-    pageIsRendering = true;
+  pageIsRendering = true;
 
-    // Get the page
-    pdfDoc.getPage(num).then((page) => {
-        // Set scale
-        const viewPort = page.getViewport({ scale })
-    })
-}
+  // Get page
+  pdfDoc.getPage(num).then((page) => {
+    // Set scale
+    const viewport = page.getViewport({ scale });
+    canvas.height = viewport.height;
+    canvas.width = viewport.width;
+
+    const renderCtx = {
+      canvasContext: ctx,
+      viewport,
+    };
+
+    page.render(renderCtx).promise.then(() => {
+      pageIsRendering = false;
+
+      if (pageNumIsPending !== null) {
+        renderPage(pageNumIsPending);
+        pageNumIsPending = null;
+      }
+    });
+
+    //output current page
+    document.querySelector("#page-num").textContent = num;
+  });
+};
+
+// Check for pages rendering
+const queueRenderPage = (num) => {
+  if (pageIsRendering) {
+    pageNumIsPending = num;
+  } else {
+    renderPage(num);
+  }
+};
+
+// Show Prev Page
+const showPrevPage = () => {
+  if (pageNum <= 1) {
+    return;
+  }
+  pageNum--;
+  queueRenderPage(pageNum);
+};
+
+// Show Next Page
+const showNextPage = () => {
+  if (pageNum >= pdfDoc.numPages) {
+    return;
+  }
+  pageNum++;
+  queueRenderPage(pageNum);
+};
 
 // Get the document
-pdfjsLib.getDocument(url).promise.then((_pdfDoc) => {
+pdfjsLib
+  .getDocument(url)
+  .promise.then((_pdfDoc) => {
     pdfDoc = _pdfDoc;
-    
-    document.querySelector('#page-count').textContent = pdfDoc.numPages;
+
+    document.querySelector("#page-count").textContent = pdfDoc.numPages;
 
     renderPage(pageNum);
-});
+  })
+  .catch((err) => {
+    // Display error
+    const div = document.createElement("div");
+    div.className = "error";
+    div.appendChild(document.createTextNode(err.message));
+    document.querySelector("body").insertBefore(div, canvas);
+    // Remove top bar
+    document.querySelector(".top-bar").style.display = "none";
+  });
+
+// Button Events
+document.querySelector("#prev-page").addEventListener("click", showPrevPage);
+document.querySelector("#next-page").addEventListener("click", showNextPage);
